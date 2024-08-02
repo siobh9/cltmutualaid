0b84151c92ae346382795bc0bc378120692d1d22
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-08-02 12:00:30 -0400

    chore: add how to guides

--- vocs.config.ts
+++ vocs.config.ts
@@ -60,5 +60,19 @@ export default defineConfig({
         },
       ]
     },
+    {
+      text: 'How To Guides',
+      collapsed: false,
+      items: [
+        {
+          text: 'How To Set Up An Open Collective',
+          link: '/how-to-guides/how-to-set-up-an-open-collective',
+        },
+        {
+          text: 'How To Make A Site Like This',
+          link: '/how-to-guides/how-to-make-a-site-like-this',
+        }
+      ]
+    },
   ],
 })
